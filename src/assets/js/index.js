40b91a1b19093ac7fa72ef6f4b3ddd810ae78bf6
Aleksandr Sadov <sascha.sadov@yandex.ru>
2021-06-06 19:52:00 +0300

    slow animation & set hidden for body

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,13 +2,15 @@ import "../scss/index.scss";
 
 const btnOpen = document.getElementById("btn-open");
 const btnClose = document.getElementById("btn-close");
-const app = document.getElementById("app");
+const { body } = document;
 const menu = document.getElementById("menu");
 
 btnOpen.addEventListener("click", function () {
+  body.classList.add("-hidden");
   menu.classList.add("-open");
 });
 
 btnClose.addEventListener("click", function () {
+  body.classList.remove("-hidden");
   menu.classList.remove("-open");
 });
