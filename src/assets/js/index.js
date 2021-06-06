a48bf64bf38da020534612c140ad1694bb2dc5b5
Aleksandr Sadov <sascha.sadov@yandex.ru>
2021-06-06 16:18:54 +0300

    update gradient & code pretty

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,14 +1,14 @@
-import '../scss/index.scss'
+import "../scss/index.scss";
 
-const btnOpen = document.getElementById('btn-open')
-const btnClose = document.getElementById('btn-close')
-const app = document.getElementById('app')
-const menu = document.getElementById('menu')
+const btnOpen = document.getElementById("btn-open");
+const btnClose = document.getElementById("btn-close");
+const app = document.getElementById("app");
+const menu = document.getElementById("menu");
 
-btnOpen.addEventListener('click', function () {
-    menu.classList.add('-open')
-})
+btnOpen.addEventListener("click", function () {
+  menu.classList.add("-open");
+});
 
-btnClose.addEventListener('click', function () {
-    menu.classList.remove('-open')
-})
+btnClose.addEventListener("click", function () {
+  menu.classList.remove("-open");
+});
